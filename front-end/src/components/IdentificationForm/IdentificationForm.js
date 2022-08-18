7107f720287a77d4b424a98c5cacb0cff40eab53
Thomas <thomasdebray83@gmail.com>
2022-08-18 18:29:55 +0200

    add div break

--- front-end/src/components/IdentificationForm/IdentificationForm.js
+++ front-end/src/components/IdentificationForm/IdentificationForm.js
@@ -1,66 +1,46 @@
 import React, { useState } from "react";
-import Button from "./Button/Button";
-import Input from "./Input/Input";
 import Link from "./Link/Link";
+import "./IdentificationForm.scss";
 import Tab from "./Tabs/Tab";
-import "./identification-form.scss";
+import Form from "./Form/Form";
 
 const IdentificationForm = () => {
+  const [formLogin, setFormLogin] = useState("login");
+  const [formRegister, setFormRegister] = useState("register");
 
-  // const tabGroup = () =>  {
-  //   const [active, setActive] = useState();
-  // }
+  const displayRegister = () => {
+    setFormRegister("register");
+    setFormLogin(null);
+  };
 
-    
+  const displayLogin = () => {
+    setFormLogin("login");
+    setFormRegister(null);
+  };
 
   return (
-    <div className="identification-form">
+    <div className="identification-container">
       <div className="register-or-login">
-        <Tab title="connexion" link="/connexion" />
-        <Tab title="inscription" link="/inscription" />
+        <Tab onClick={displayRegister}>Inscription</Tab>
+        <div className="break" />
+        <Tab onClick={displayLogin}>Connexion</Tab>
       </div>
-      <form className="form">
-        <Input
-          type="text"
-          placeholder="Prénom"
-          id="firstname"
-          name="firstname"
-          min="2"
-          max="40"
-        />
-        <Input
-          type="text"
-          placeholder="Nom"
-          id="lastname"
-          name="lastname"
-          min="2"
-          max="60"
-        />
-        <Input
-          type="email"
-          placeholder="Votre adresse mail"
-          id="email"
-          name="email"
-        />
-        <Input
-          type="password"
-          placeholder="Mot de passe"
-          id="password"
-          name="password"
-          min="10"
-          max="32"
-        />
-        <Input
-          type="password"
-          placeholder="Confirmez le mot de passe"
-          id="verify-password"
-          name="verify-password"
-          min="10"
-          max="32"
-        />
-        <Button />
-        <Link />
-      </form>
+      {formRegister === "register" ? (
+        <>
+          <Form form="register" />
+          <div className="links">
+            <Link content="Vous avez déjà un compte ?" onClick={displayLogin} />
+          </div>
+        </>
+      ) : (
+        <>
+          <Form form="login" />
+          <div className="links">
+            <Link content="Mot de passe oublié ?" onClick={displayRegister} />
+            <Link content="Pas encore de compte ?" onClick={displayRegister} />
+          </div>
+        </>
+      )}
     </div>
   );
 };
