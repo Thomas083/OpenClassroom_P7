aac54286430be55ac49ca881006c17f0999791a5
Thomas <thomasdebray83@gmail.com>
2022-08-21 14:24:16 +0200

    Possible to modify his profile but not yet the photo

--- back-end/controllers/user.controller.js
+++ back-end/controllers/user.controller.js
@@ -4,7 +4,6 @@ const db = dbc.getDB();
 // RUD users
 
 exports.getOneUser = (req, res, next) => {
-  console.log(req.params.id);
   const { id: userId } = req.params;
   const sqlGetEmail = `SELECT user_email FROM users WHERE users.user_id = ${userId};`;
   db.query(sqlGetEmail, (err, result) => {
@@ -14,6 +13,21 @@ exports.getOneUser = (req, res, next) => {
     }
     if (result[0]) {
       res.status(200).json(result[0].user_email);
+    }
+  });
+};
+
+exports.updateOneUser = (req, res, next) => {
+  const { user_firstname, user_lastname } = req.body;
+  const { id: userId } = req.params;
+  const sqlUpdateUser = `UPDATE users SET user_firstname = "${user_firstname}", user_lastname = "${user_lastname}" WHERE users.user_id = ${userId};`;
+  db.query(sqlUpdateUser, (err, result) => {
+    if (err) {
+      res.status(404).json({ err });
+      throw err;
+    }
+    if (result) {
+      res.status(200).json(result);
     }
   });
 };
